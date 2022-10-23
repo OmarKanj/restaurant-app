a7bd204b3742daada87ca7c7780270513d252942
Omar Kanj <omar.kanj99@gmail.com>
2022-10-23 15:28:54 -0700

    added init files

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react'
+import {SidebarItems} from './SidebarData.js'
+import './Sidebar.css'
+import {Button} from '../Button.js'
+
+class Sidebar extends Component {
+    state = {
+        clicked: false
+    }
+
+    handleClick = () => {
+        this.setState({
+            clicked: !this.state.clicked
+        })
+    }
+
+  render() { return (
+    <nav className='sidebar'>
+        <h1 className='sidebar-logo'> 
+            The Garden Mediterranean Grill
+        </h1>
+        <div className='sidebar-button-mobile'>
+            <button className='sidebar-button' onClick={this.handleClick}>
+                <i className={this.state.clicked ? 'fa-solid fa-times':'fa-solid fa-bars'}></i>
+            </button>
+        </div>
+        <ul className={this.state.clicked ? 'sidebar-items active': 'sidebar-items'}>
+            {SidebarItems.map((item, index) => {
+                return (
+                    <li key={index}>
+                        <a className={item.cName} href={item.href}>
+                        <i className={`${item.icon} sidebar-icon-settings`}></i>
+                        {item.name}
+                        </a>
+                    </li>
+                )
+            })}
+        </ul>
+    </nav>
+    )
+    }
+}
+
+export default Sidebar
---
+++ src/components/Sidebar/SidebarData.js
@@ -0,0 +1,32 @@
+export const SidebarItems = [
+    {
+        icon: "fa-solid fa-house",
+        name: "Home",
+        cName: "sidebar-item",
+        href: "/"
+    },
+    {
+        icon: "fa-solid fa-utensils",
+        name: "Menu",
+        cName: "sidebar-item",
+        href: "/menu"
+    },
+    {
+        icon: "fa-solid fa-phone",
+        name: "Contact us",
+        cName: "sidebar-item",
+        href: "/contact"
+    },
+    {
+        icon: "fa-solid fa-globe",
+        name: "Order online",
+        cName: "sidebar-item",
+        href: "/order"
+    },
+    {
+        icon: "fa-solid fa-info",
+        name: "About us",
+        cName: "sidebar-item",
+        href: "/about"
+    },
+]
